25f7ee47d28f56ecd8eb797451027ef6a2c9436d
MortenB22R <49730926+mortensb1@users.noreply.github.com>
2023-09-04 14:27:53 +0200

    Movement

--- sketch.js
+++ sketch.js
@@ -6,9 +6,6 @@ let prikSk = [0,0];
 
 let i = 0;
 
-let lastX = 0;
-let lastY = 0;
-
 function setup() {
   createCanvas(400, 400);
 }
@@ -23,53 +20,87 @@ function draw() {
 
   // point(prikSk[0],prikSk[1]);
 
-  i -= 0.1;
+  // i -= 1;
 
+  Prik3D(-100,-100,-100+i);
+  Prik3D(-100,100,-100+i);
   Prik3D(100,100,-100+i);
-  Prik3D(100,200,-100+i);
-  Prik3D(200,200,-100+i);
-  Prik3D(200,100,-100+i);
+  Prik3D(100,-100,-100+i);
 
-  Line3D(100,100,-100+i,100,200,-100+i);
-  Line3D(100,200,-100+i,200,200,-100+i)
-  Line3D(200,200,-100+i,200,100,-100+i)
-  Line3D(200,100,-100+i,100,100,-100+i)
+
+  Line3D(-100,-100,-100+i,-100,100,-100+i);
+  Line3D(-100,100,-100+i,100,100,-100+i)
+  Line3D(100,100,-100+i,100,-100,-100+i)
+  Line3D(100,-100,-100+i,-100,-100,-100+i)
   
+  Prik3D(-100,-100,-200+i);
+  Prik3D(-100,100,-200+i);
   Prik3D(100,100,-200+i);
-  Prik3D(200,100,-200+i);
-  Prik3D(200,200,-200+i);
-  Prik3D(100,200,-200+i);
+  Prik3D(100,-100,-200+i);
 
-  Line3D(100,100,-200+i,100,200,-200+i);
-  Line3D(100,200,-200+i,200,200,-200+i)
-  Line3D(200,200,-200+i,200,100,-200+i)
-  Line3D(200,100,-200+i,100,100,-200+i)
+  Line3D(-100,-100,-200+i,-100,100,-200+i);
+  Line3D(-100,100,-200+i,100,100,-200+i)
+  Line3D(100,100,-200+i,100,-100,-200+i)
+  Line3D(100,-100,-200+i,-100,-100,-200+i)
 
+  Line3D(-100,-100,-200+i,-100,-100,-100+i)
+  Line3D(-100,100,-200+i,-100,100,-100+i)
   Line3D(100,100,-200+i,100,100,-100+i)
-  Line3D(200,100,-200+i,200,100,-100+i)
-  Line3D(100,200,-200+i,100,200,-100+i)
-  Line3D(200,200,-200+i,200,200,-100+i)
+  Line3D(100,-100,-200+i,100,-100,-100+i)
+
+  if (keyIsDown(65)) //a
+  {
+    camPos[0] += 4;
+  }
+  if (keyIsDown(68)) //d
+  {
+    camPos[0] -= 4;
+  }
+  if (keyIsDown(87)) //w
+  {
+    camPos[1] += 4;
+  }
+  if (keyIsDown(83)) //s
+  {
+    camPos[1] -= 4;
+  }
+  if (keyIsDown(40)) //down arrow
+  {
+    camPos[2] -= 2
+  }
+  if (keyIsDown(38)) //up arrow
+  {
+    camPos[2] += 2
+  }
+
 }
 
 function Prik3D (prikX,prikY,prikZ)
 {
+  prikX += camPos[0];
+  prikY += camPos[1];
+  prikZ += camPos[2];
+
   prikPositionSkermX = (prikX*cS)/prikZ;
   prikPositionSkermY = (prikY*cS)/prikZ;
 
-  point(prikPositionSkermX,prikPositionSkermY);
-  // line(lastX,lastY,prikPositionSkermX,prikPositionSkermY)
-
-  // lastX = prikPositionSkermX;
-  // lastY = prikPositionSkermY;
+  point(prikPositionSkermX + width/2,prikPositionSkermY + height/2);
 }
 
 function Line3D (lineX1,lineY1,lineZ1,lineX2,lineY2,lineZ2)
 {
+  lineX1 += camPos[0];
+  lineX2 += camPos[0];
+  lineY1 += camPos[1];
+  lineY2 += camPos[1];
+  lineZ1 += camPos[2];
+  lineZ2 += camPos[2];
+
   linePositionSkermX1 = (lineX1*cS)/lineZ1;
   linePositionSkermY1 = (lineY1*cS)/lineZ1;
 
   linePositionSkermX2 = (lineX2*cS)/lineZ2;
   linePositionSkermY2 = (lineY2*cS)/lineZ2;
 
-  line(linePositionSkermX1,linePositionSkermY1,linePositionSkermX2,linePositionSkermY2)
+  line(linePositionSkermX1 + width/2,linePositionSkermY1 + height/2,linePositionSkermX2 + width/2,linePositionSkermY2 + height/2)
 }
